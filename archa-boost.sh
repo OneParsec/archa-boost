3f3d65bc404cd6e9211d0cc123dad35ef2187963
Alexandr Simonov <asimonov1998@gmail.com>
2020-10-28 20:12:51 +0300

    Add files via upload

--- archa-boost.sh
+++ archa-boost.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/bash
 if [[ "$EUID" -eq 0 ]]; then
 	echo "This script must be run as user"
 	sleep 2
@@ -78,6 +77,18 @@ elif [[ $option = 5 ]]; then
 	sleep 3
 	exit
 elif [[ $option = 6 ]]; then
+	echo "You choose Catalyst AMD Video Driver"
+	sleep 1
+	sudo pacman -Syu git --needed
+	git clone https://aur.archlinux.org/catalyst.git
+	cd catalyst
+	makepkg -si
+	echo ""
+	clear
+	echo "Catalyst AMD Video Driver successfuly installed."
+	sleep 3
+	exit
+elif [[ $option = 7 ]]; then
 	echo "You choose Feral Gamemode"
 	sleep 1
 	sudo pacman -Syu meson systemd git dbus libinih
@@ -90,20 +101,33 @@ elif [[ $option = 6 ]]; then
 	echo "Feral Gamemode successfuly installed."
 	sleep 3
 	exit
-elif [[ $option = 7 ]]; then
+elif [[ $option = 8 ]]; then
 	echo "You choose Proton-5.9-GE-8-ST"
 	sleep 1
 	mkdir ~/.steam/root/compatibilitytools.d
 	sudo pacman -Syu wget steam --needed
 	wget https://github.com/GloriousEggroll/proton-ge-custom/releases/download/5.9-GE-8-ST/Proton-5.9-GE-8-ST.tar.gz
 	tar -xvf Proton-5.9-GE-8-ST.tar.gz
-	cp Proton-5.9-GE-8-ST ~/.steam/root/compatibilitytools.d/
+	cp -r Proton-5.9-GE-8-ST ~/.steam/root/compatibilitytools.d/
 	echo ""
 	clear
 	echo "Proton-5.9-GE-8-ST successfuly installed. Please, restart Steam."
 	sleep 3
 	exit
-elif [[ $option = 8 ]]; then
+elif [[ $option = 9 ]]; then
+	echo "You choose Proton-5.9-GE-8-ST (for flatpak Steam)"
+	sleep 1
+	mkdir ~/.var/app/com.valvesoftware.Steam/data/Steam/compatibilitytools.d/
+	sudo pacman -Syu wget --needed
+	wget https://github.com/GloriousEggroll/proton-ge-custom/releases/download/5.9-GE-8-ST/Proton-5.9-GE-8-ST.tar.gz
+	tar -xvf Proton-5.9-GE-8-ST.tar.gz
+	cp -r Proton-5.9-GE-8-ST ~/.var/app/com.valvesoftware.Steam/data/Steam/compatibilitytools.d/
+	echo ""
+	clear
+	echo "Proton-5.9-GE-8-ST (for flatpak Steam) successfuly installed. Please, restart Steam."
+	sleep 3
+	exit
+elif [[ $option = 10 ]]; then
 	echo "You choose PortProton-3.4"
 	sleep 1
 	sudo pacman -Syu cabextract cups faudio lib32-acl lib32-faudio lib32-fontconfig lib32-freetype2 lib32-gettext lib32-giflib lib32-gnutls lib32-gst-plugins-base-libs lib32-gtk3 lib32-harfbuzz lib32-lcms2 lib32-libjpeg-turbo lib32-libldap lib32-libnl lib32-libpcap lib32-libpng lib32-libtasn1 lib32-libtiff lib32-libusb lib32-libxcomposite lib32-libxinerama lib32-libxrandr lib32-libxslt lib32-libxss lib32-mpg123 lib32-nspr lib32-nss lib32-opencl-icd-loader lib32-p11-kit lib32-sqlite lib32-v4l-utils lib32-vkd3d lib32-vulkan-icd-loader libimagequant lsof opencl-icd-loader python-distro python-evdev python-pillow sane vkd3d zenity icoutils xterm wget curl libudev0-shim python2 wxgtk-common wxgtk3 gnu-netcat lib32-libudev0-shim lib32-vulkan-intel lib32-vulkan-radeon python2-wxpython3 vulkan-intel vulkan-tools vulkan-radeon opencl-nvidia lib32-opencl-nvidia lib32-nvidia-utils
@@ -113,7 +137,7 @@ elif [[ $option = 8 ]]; then
 	echo "PortProton-3.4 successfuly installed."
 	sleep 3
 	exit
-elif [[ $option = 9 ]]; then
+elif [[ $option = 11 ]]; then
 	echo "Goodbye!"
 	exit
 fi
